9ad5dbe6897e19f3837d14cb29d01d02120a9f22
Christopher Stuart Mellor <Chris.Mellor@hotmail.co.uk>
2022-05-08 23:05:25 +0100

    Added styling for posts

--- src/DSSL.Blog.Web/Pages/Index.js
+++ src/DSSL.Blog.Web/Pages/Index.js
@@ -1,5 +1,14 @@
 $(function () {
     var l = abp.localization.getResource("Blog");
 
+    var createModal = new abp.ModalManager(abp.appPath + 'Comments/CreateModal');
 
+    createModal.onResult(function () {
+
+    });
+
+    $('#NewCommentButton').click(function (e) {
+        e.preventDefault();
+        createModal.open();
+    });
 });
