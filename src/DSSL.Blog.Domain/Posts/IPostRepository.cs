fb44b1523889168cc5deae9e93c1d3a0f74831b6
Christopher Stuart Mellor <Chris.Mellor@hotmail.co.uk>
2022-05-09 21:46:33 +0100

    Added interfaces

---
+++ src/DSSL.Blog.Domain/Posts/IPostRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Repositories;
+
+namespace Dssl.Blog.Posts
+{
+    public interface IPostRepository : IBasicRepository<Post, Guid>
+    {
+        Task<ICollection<Post>> GetPosts();
+        Task<ICollection<Post>> GetPostsByOrder(bool descending);
+    }
+}
---
+++ src/DSSL.Blog.Domain/Tags/ITagRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Repositories;
+
+namespace Dssl.Blog.Tags
+{
+    public interface ITagRepository : IBasicRepository<Tag, Guid>
+    {
+        Task<ICollection<Tag>> GetTagsAsync();
+        Task<Tag> GetByNameAsync(string name);
+    }
+}
